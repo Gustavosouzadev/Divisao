c6974ba4162c71617a04477097f6d8ca9d51a314
Gustavosouzadev <gustavoduti2003@gmail.com>
2021-04-25 15:28:40 -0300

    Ajustes

--- Program.cs
+++ Program.cs
@@ -10,36 +10,65 @@ namespace Divisao
 
             int Numerador;
             int Denominador; 
+            string QuantidadeDigitada; 
+            string letras;
+            bool NumeroValido;
+            bool NumeroValido2;
 
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Black;
             Console.WriteLine("-----Bem-Vindo-----\n");
 
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.Write("Digite o Numerador....: ");
 
             Console.ForegroundColor = ConsoleColor.White;
-            Numerador = Convert.ToInt32(Console.ReadLine());
+            letras = Console.ReadLine();
+            NumeroValido2 = Int32.TryParse(letras, out Numerador);
 
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.Write("Digite o Denominador..: ");
 
             Console.ForegroundColor = ConsoleColor.White;
-            Denominador = Convert.ToInt32(Console.ReadLine());
-
-             if (Denominador == 0)
+            QuantidadeDigitada = Console.ReadLine();
+            NumeroValido = Int32.TryParse(QuantidadeDigitada, out Denominador);
+            
+            if (!NumeroValido2)
             {
                 Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.Write("\nERROR: ");
 
                 Console.ForegroundColor = ConsoleColor.White;
-                Console.Write("Não é possível dividir por zero.");
-                Environment.Exit(-1);
+                Console.Write("Só dividimos Números.");
             }
+            else
+            {  
+                if (!NumeroValido)
+                {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write("\nERROR: ");
 
-            int Resultado = Numerador / Denominador;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Só Dividimos Números.");
+                }
+                else
+                {
+                    if (Denominador == 0)
+                    {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write("\nERROR: ");
 
-            Console.Write($"\n {Numerador} dividido por {Denominador} é {Resultado}.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("Não é possível dividir por zero.");
+                    Environment.Exit(-1);
+                    }
+                    else
+                    {
+                    int Resultado = Numerador / Denominador;
 
+                    Console.Write($"\n {Numerador} dividido por {Denominador} é {Resultado}.");
+                    }
+                }    
+            }            
             Console.ResetColor();
         }
     }
